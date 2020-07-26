85ae970c7a7cfe46f6d2dd45b66bd1898e7c7fcb
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-07-26 11:35:18 +0900

    basically made all the form style working

--- src/App.vue
+++ src/App.vue
@@ -48,18 +48,18 @@
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
           <label for="male">
-            <input type="radio" id="male" value="Male" /> Male
+            <input type="radio" id="male" value="Male" v-model="gender" /> Male
           </label>
           <label for="female">
-            <input type="radio" id="female" value="Female" /> Female
+            <input type="radio" id="female" value="Female" v-model="gender" /> Female
           </label>
         </div>
       </div>
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
           <label for="priority">Priority</label>
-          <select id="priority" class="form-control">
-            <option></option>
+          <select id="priority" class="form-control" v-model="chosenPriority">
+            <option v-for="(item, index) in priorities" :key="index">{{item}}</option>
           </select>
         </div>
       </div>
@@ -91,8 +91,8 @@
             <ul>
               <li v-for="item in sendMail" :key="item.index">{{item}}</li>
             </ul>
-            <p>Gender:</p>
-            <p>Priority:</p>
+            <p>Gender: {{gender}}</p>
+            <p>Priority: {{chosenPriority}}</p>
             <p>Switched:</p>
           </div>
         </div>
@@ -112,6 +112,9 @@ export default {
       },
       message: "New message",
       sendMail: [],
+      gender: "Male",
+      priorities: ["high", "medium", "low"],
+      chosenPriority: "high",
     };
   },
 };
